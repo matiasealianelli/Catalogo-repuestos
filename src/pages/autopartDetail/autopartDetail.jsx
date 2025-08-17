8bf4af06da69c51cb182aec0fcfc1c093ccd9407
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-08-16 23:29:29 -0300

    Refactor AutopartDetail component layout and styles; add navigation link and price display

--- src/pages/autopartDetail/autopartDetail.jsx
+++ src/pages/autopartDetail/autopartDetail.jsx
@@ -8,30 +8,43 @@ export default function AutopartDetail() {
   const { SKU } = useParams();
   const part = autopartsData.find((part) => part.SKU === SKU);
   if (!part) {
-    return <p>No se puede encontrar ningun repuesto</p>;
+    return (<>
+      <Link to={"/repuestos"} className="btn btn-light"><i class="bi bi-arrow-left-circle-fill"></i> Volver a repuestos</Link>
+      <p>No se puede encontrar ningun repuesto</p>;
+    </>)
   }
   return (
     <>
       <Layout>
         <section className="autopartDetail" id="autopartDetail">
+          <Link to={"/repuestos"} className="btn btn-light btnBackAutoparts"><i class="bi bi-arrow-left-circle"></i> Volver a repuestos</Link>
           <div className="conteinerDetailAutopart">
-            <div>
+            <div className="col1AutopartDetail">
               <img
                 className="autopartImageDetail"
                 src={part.IMAGE}
                 alt={part.REPUESTO}
               />
             </div>
-            <div className="conteinerFeaturesAutoparts">
-              <h1 className="titleDetailAutopart"> {part.REPUESTO} {part.LADO_PIEZA} {part.MARCA_REPUESTO} {part.CARACTERISTICAS_EXTRA} {part.MARCA_VEHICULO} {part.MODELO_VEHICULO} {part.AÑOS_COMPATIBLE} </h1>
-              <ContentFeatureAutopart property={"SKU"} value={part.SKU} />
-              <ContentFeatureAutopart property={"Marca repuesto"} value={part.MARCA_REPUESTO} />
-              <ContentFeatureAutopart property={"Repuesto"} value={part.REPUESTO} />
-              <ContentFeatureAutopart property={"Stock disponible"} value={part.STOCK} />
-              <ContentFeatureAutopart property={"Lado pieza"} value={part.LADO_PIEZA} />
-              <ContentFeatureAutopart property={"Vehiculo"} value={part.MARCA_VEHICULO} />
-              <ContentFeatureAutopart property={"Modelo vehiculo"} value={part.MODELO_VEHICULO} />
-              <ContentFeatureAutopart property={"Años compatible"} value={part.AÑOS_COMPATIBLE} />
+            <div className="col2AutopartDetail">
+               <h1 className="titleDetailAutopart"> {part.REPUESTO} {part.LADO_PIEZA} {part.MARCA_VEHICULO} {part.MODELO_VEHICULO} {part.AÑOS_COMPATIBLE} </h1>
+              <div className="conteinerBoxFeaturesBoxBuy">
+                <div className="conteinerBuy">
+                  <h2 className="priceAutopart" id="priceAutopart">$ {part.PRICE} </h2>
+                  <button className="btn btn-secondary">Agregar al carrito</button>
+                  <button className="btn btn-light">Consultar al vendedor</button>
+                </div>  
+                <div className="conteinerFeaturesAutoparts">
+                  <ContentFeatureAutopart property={"SKU"} value={part.SKU} />
+                  <ContentFeatureAutopart property={"Marca repuesto"} value={part.MARCA_REPUESTO} />
+                  <ContentFeatureAutopart property={"Repuesto"} value={part.REPUESTO} />
+                  <ContentFeatureAutopart property={"Stock disponible"} value={part.STOCK} />
+                  <ContentFeatureAutopart property={"Lado pieza"} value={part.LADO_PIEZA} />
+                  <ContentFeatureAutopart property={"Vehiculo"} value={part.MARCA_VEHICULO} />
+                  <ContentFeatureAutopart property={"Modelo vehiculo"} value={part.MODELO_VEHICULO} />
+                  <ContentFeatureAutopart property={"Años compatible"} value={part.AÑOS_COMPATIBLE} />
+                </div>
+              </div>
             </div>
           </div>
         </section>

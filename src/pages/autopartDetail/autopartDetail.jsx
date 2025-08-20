6f5a3fe61b8fea6379aa6a4ada9f7ff268001523
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-08-20 17:14:35 -0300

    Refactor autopartDetail page layout and integrate Swiper for image display

--- src/pages/autopartDetail/autopartDetail.jsx
+++ src/pages/autopartDetail/autopartDetail.jsx
@@ -1,48 +1,118 @@
 import "./autopartDetail.css";
 import { Link, useParams } from "react-router-dom";
-import autopartsData from "../../../public/data/autoparts-data/autoparts.json";
+import autoparts from "../../../data/autoparts.json";
 import Layout from "../../components/layout/layout";
 import ContentFeatureAutopart from "../../components/ui/components-autoparts/autopartsDetail/contentFeatureAutopart";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function AutopartDetail() {
-  const { SKU } = useParams();
-  const part = autopartsData.find((part) => part.SKU === SKU);
-  if (!part) {
-    return (<>
-      <Link to={"/repuestos"} className="btn btn-light"><i class="bi bi-arrow-left-circle-fill"></i> Volver a repuestos</Link>
-      <p>No se puede encontrar ningun repuesto</p>;
-    </>)
+  const { ID } = useParams();
+  const autopart = autoparts.find((autopart) => autopart.ID === ID);
+  if (!autopart) {
+    return (
+      <>
+        <Link to={"/repuestos"} className="btn btn-light">
+          <i className="bi bi-arrow-left-circle"></i> Volver
+        </Link>
+        <p>No se puede encontrar ningun repuesto</p>
+      </>
+    );
   }
   return (
     <>
       <Layout>
         <section className="autopartDetail" id="autopartDetail">
-          <Link to={"/repuestos"} className="btn btn-light btnBackAutoparts"><i class="bi bi-arrow-left-circle"></i> Volver a repuestos</Link>
+          <div className="conteinerBtnBackAutoparts">
+            <Link to={"/repuestos"} className="btn btn-light btnBackAutoparts">
+              <i className="bi bi-arrow-left-circle"></i> Volver
+            </Link>
+          </div>
           <div className="conteinerDetailAutopart">
-            <div className="col1AutopartDetail">
-              <img
-                className="autopartImageDetail"
-                src={part.IMAGE}
-                alt={part.REPUESTO}
-              />
-            </div>
+            <Swiper
+              className="col1AutopartDetail"
+              modules={[Pagination, Navigation]}
+              scrollbar={{ draggable: true }}
+              centeredSlides={true}
+              autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+              }}
+              pagination={true}
+              Navigation={true}
+              loop={true}
+              Pagination={{
+                clickable: true,
+              }}
+              navigation={true}>
+              {autopart
+                ? autopart.IMAGES.map((image, index) => (
+                    <SwiperSlide className="conteinerSlide">
+                      <img
+                        className="autopartImageDetail"
+                        key={index}
+                        src={image}
+                        alt=""
+                      />
+                    </SwiperSlide>
+                  ))
+                : "/assets/img/no-image.png"}
+            </Swiper>
             <div className="col2AutopartDetail">
-               <h1 className="titleDetailAutopart"> {part.REPUESTO} {part.LADO_PIEZA} {part.MARCA_VEHICULO} {part.MODELO_VEHICULO} {part.AÑOS_COMPATIBLE} </h1>
-              <div className="conteinerBoxFeaturesBoxBuy">
+              <h1 className="titleDetailAutopart">
+                {" "}
+                {autopart.REPUESTO} {autopart.LADO_REPUESTO}{" "}
+                {autopart.MARCA_VEHICULO} {autopart.MODELO_VEHICULO}{" "}
+                {autopart.AÑOS_COMPATIBLE}{" "}
+              </h1>
+              <div className="conteinerFeaturesBuy">
                 <div className="conteinerBuy">
-                  <h2 className="priceAutopart" id="priceAutopart">$ {part.PRICE} </h2>
-                  <button className="btn btn-secondary">Agregar al carrito</button>
-                  <button className="btn btn-light">Consultar al vendedor</button>
-                </div>  
+                  <h2 className="priceAutopart" id="priceAutopart">
+                    $ {autopart.PRICE}{" "}
+                  </h2>
+                  <button className="btn btn-secondary">
+                    Agregar al carrito
+                  </button>
+                  <button className="btn btn-light">
+                    Consultar al vendedor
+                  </button>
+                </div>
                 <div className="conteinerFeaturesAutoparts">
-                  <ContentFeatureAutopart property={"SKU"} value={part.SKU} />
-                  <ContentFeatureAutopart property={"Marca repuesto"} value={part.MARCA_REPUESTO} />
-                  <ContentFeatureAutopart property={"Repuesto"} value={part.REPUESTO} />
-                  <ContentFeatureAutopart property={"Stock disponible"} value={part.STOCK} />
-                  <ContentFeatureAutopart property={"Lado pieza"} value={part.LADO_PIEZA} />
-                  <ContentFeatureAutopart property={"Vehiculo"} value={part.MARCA_VEHICULO} />
-                  <ContentFeatureAutopart property={"Modelo vehiculo"} value={part.MODELO_VEHICULO} />
-                  <ContentFeatureAutopart property={"Años compatible"} value={part.AÑOS_COMPATIBLE} />
+                  <ContentFeatureAutopart
+                    property={"SKU"}
+                    value={autopart.SKU}
+                  />
+                  <ContentFeatureAutopart
+                    property={"Marca repuesto"}
+                    value={autopart.MARCA_REPUESTO}
+                  />
+                  <ContentFeatureAutopart
+                    property={"Repuesto"}
+                    value={autopart.REPUESTO}
+                  />
+                  <ContentFeatureAutopart
+                    property={"Stock disponible"}
+                    value={autopart.STOCK}
+                  />
+                  <ContentFeatureAutopart
+                    property={"Lado pieza"}
+                    value={autopart.LADO_REPUESTO}
+                  />
+                  <ContentFeatureAutopart
+                    property={"Vehiculo"}
+                    value={autopart.MARCA_VEHICULO}
+                  />
+                  <ContentFeatureAutopart
+                    property={"Modelo vehiculo"}
+                    value={autopart.MODELO_VEHICULO}
+                  />
+                  <ContentFeatureAutopart
+                    property={"Años compatible"}
+                    value={autopart.AÑOS_COMPATIBLE}
+                  />
                 </div>
               </div>
             </div>
